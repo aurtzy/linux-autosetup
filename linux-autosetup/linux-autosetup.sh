3c8f99ac8c0035aa01d0efbe898df401b18c0700
aurtzy <65694715+aurtzy@users.noreply.github.com>
2021-09-04 01:25:43 -0400

    Conglomeration of stuff, add app & rec class
    - Introduce app & rec classes, and dump old files into 'old' folder for porting still-useful stuff then deleting later.
    - Added basic config file with no functionality with script yet.

--- linux-autosetup/linux-autosetup.sh
+++ linux-autosetup/linux-autosetup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # OOP emulation credit to Maxim Norin from https://stackoverflow.com/questions/36771080/creating-classes-and-objects-using-bash-scripting#comment115718570_40981277
-# include class header
+# source app and rec class files
 . app.h
+. rec.h
+
+# Import everything from config!!
+. config
+
 
 app java
+java.constructor 
 
-java.fileName = yes
-java.fileName
--- linux-autosetup/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-name="java"
-echo $name' is the name of the app.'
-declare $name.hello = 'hello'
-$name.hello = "hi."
-echo $(java.hello)
