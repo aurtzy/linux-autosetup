0c21024c6e8ae02708f872f8941e7a7d722f7fe8
aurtzy <65694715+aurtzy@users.noreply.github.com>
2021-09-04 17:01:51 -0400

    Implement default/custom app install capability
    - Documented use of defaultInstallCommand in config
    Misc.
    - Added fallbacks APP_BACKUP_DIR REC_BACKUP_DIR defaultInstallCommand
    - Renamed backupFileDirs to backupSources
    - Decided to make backupSources a single string that will be parsed by script

--- linux-autosetup/linux-autosetup.sh
+++ linux-autosetup/linux-autosetup.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Default fallback variables if config goes wrong somehow
+
+# where application backups go
+declare APP_BACKUP_DIR=./app-backups
+# where recovery files go
+declare REC_BACKUP_DIR=./recovery
+# default install command used if one is not specified for app
+declare defaultInstallCommand='echo User must set defaultInstallCommand. "name" will not be installed until this is done.'
+
+# Initial functions
+isEmptyString() {
+	if [[ $1 == '' || $1 == '|' || $1 == '-' ]]; then
+		echo 'true'
+	else
+		echo ''
+	fi
+}
+
 # OOP emulation credit to Maxim Norin from https://stackoverflow.com/questions/36771080/creating-classes-and-objects-using-bash-scripting#comment115718570_40981277
 # source app and rec class files
 . classes/app.h
@@ -11,5 +29,5 @@
 
 
 app java
-java.constructor 
+java.constructor
 
