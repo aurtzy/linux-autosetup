5d8b2b8a645af56318ec3d2f9da673026132c933
aurtzy <65694715+aurtzy@users.noreply.github.com>
2021-09-04 20:11:10 -0400

    More properly attribute credit

--- linux-autosetup/linux-autosetup.sh
+++ linux-autosetup/linux-autosetup.sh
@@ -18,7 +18,6 @@ isEmptyString() {
 	fi
 }
 
-# OOP emulation credit to Maxim Norin from https://stackoverflow.com/questions/36771080/creating-classes-and-objects-using-bash-scripting#comment115718570_40981277
 # source app and rec class files
 . classes/app.h
 . classes/rec.h
