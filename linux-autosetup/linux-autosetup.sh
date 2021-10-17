de06b7bb349210f000a764b15a14d49139d693d9
aurtzy <65694715+aurtzy@users.noreply.github.com>
2021-10-16 20:32:09 -0400

    standardize check for duplicate names

--- linux-autosetup/linux-autosetup.sh
+++ linux-autosetup/linux-autosetup.sh
@@ -124,6 +124,26 @@ dump() {
 	fi
 }
 
+# Check for duplicate names and throw error+exit if one is found
+checkDuplicateNames() {
+	if [ "$2" = "appGroups" ]; then
+		if [[ "${apps[*]} ${archives[*]} ${!archiveGroups[*]}" =~ "$1" ]]; then
+			echo "Error: $1 has an overlapping name, please fix."
+			exit 1
+		fi
+	elif [ "$2" = "archiveGroups" ]; then
+		if [[ "${apps[*]} ${!appGroups[*]} ${archives[*]}" =~ "$1" ]]; then
+			echo "Error: $1 has an overlapping name, please fix."
+			exit 1
+		fi
+	else
+		if [[ "${apps[*]} ${!appGroups[*]} ${archives[*]} ${!archiveGroups[*]}" =~ "$1" ]]; then
+			echo "Error: $1 has an overlapping name, please fix."
+			exit 1
+		fi
+	fi
+}
+
 # String converter methods to allow functionality with Bash
 # Convert '-' and $hyphenConversion from and to each other
 convertHyphens() {
@@ -148,6 +168,7 @@ App() {
 		echo "Exiting..."
 		exit 1
 	fi
+	checkDuplicateNames "$1"
 	fields="app_$(convertHyphens "$1")_fields"
 	. <(sed "s/fields/$fields/g" <(sed "s/App/$1/g" "$CLASSES_DIR"/App.class))
 	$1.constructor "${@:2}"
@@ -166,7 +187,15 @@ AppGroup() {
 	if [ "$1" = '' ]; then
 		echo "Error: AppGroup name parameter was empty."
 		return
+	elif [[ "$1" =~ [[:blank:]] ]]; then
+		echo
+		echo "Error: app group name '$1' has whitespaces"
+		echo "App group names cannot have whitespaces (e.g. spaces, tabs)."
+		echo "Please check your config file and resolve the issue."
+		echo "Exiting..."
+		exit 1
 	fi
+	checkDuplicateNames "$1" "appGroups"
 	fields="appGroup_$(convertHyphens "$1")_fields"
 	. <(sed "s/fields/$fields/g" <(sed "s/AppGroup/$1/g" "$CLASSES_DIR"/AppGroup.class))
 	$1.constructor ${@:2}
@@ -194,6 +223,7 @@ Archive() {
 		echo "Exiting..."
 		exit 1
 	fi
+	checkDuplicateNames "$1"
 	fields="archive_$(convertHyphens "$1")_fields"
 	. <(sed "s/fields/$fields/g" <(sed "s/Archive/$1/g" "$CLASSES_DIR"/Archive.class))
 	$1.constructor "${@:2}"
@@ -206,7 +236,15 @@ ArchiveGroup() {
 	if [ "$1" = '' ]; then
 		echo "Error: ArchiveGroup name parameter was empty."
 		return
+	elif [[ "$1" =~ [[:blank:]] ]]; then
+		echo
+		echo "Error: Archive group name '$1' has whitespaces"
+		echo "Archive group names cannot have whitespaces (e.g. spaces, tabs)."
+		echo "Please check your config file and resolve the issue."
+		echo "Exiting..."
+		exit 1
 	fi
+	checkDuplicateNames "$1" "archiveGroups"
 	fields="archiveGroup_$(convertHyphens "$1")_fields"
 	. <(sed "s/fields/$fields/g" <(sed "s/ArchiveGroup/$1/g" "$CLASSES_DIR"/ArchiveGroup.class))
 	$1.constructor ${@:2}
