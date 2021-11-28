1f3f465db6bae7173960493cffafe4ee8deacef2
aurtzy <65694715+aurtzy@users.noreply.github.com>
2021-11-27 21:58:56 -0500

    bump version: v2.0.1-rc2

--- linux-autosetup/linux-autosetup.sh
+++ linux-autosetup/linux-autosetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # *** https://stackoverflow.com/questions/8455991/elegant-way-for-verbose-mode-in-scripts
-version="v2.0.0-rc1"
+version="v2.0.0-rc2"
 
 #########################
 # VARIABLE DECLARATIONS #
