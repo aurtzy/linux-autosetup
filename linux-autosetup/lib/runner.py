15392206d7249824150ff16fb5574bd381bf4299
aurtzy <65694715+aurtzy@users.noreply.github.com>
2021-12-29 19:30:07 -0500

    redo method of running commands; deprecate sudo_loop() favoring running script as sudo now after rethinking whether this actually had any major implications on security. this approach i'm going towards seems to be the most elegant, too.

--- linux-autosetup/lib/runner.py
+++ linux-autosetup/lib/runner.py
@@ -1,86 +1,75 @@
-import getpass
 import logging
 import os
 import pwd
-import threading
-import time
-from subprocess import Popen, PIPE
+import subprocess
 
 from lib.logger import log
 
 
 class Runner:
     """
-    Provides a means of running command strings and handles management of permission elevations.
+    Provides a means of running commands as another user.
 
-    Limitations:
-        Since is_root checks if the euid is 0, users may not be able to run as target users if they are not the
-        root user, even if they may have sufficient privileges to do so. This means
-        users are required to run the script as root if they want to target another user.
+    This code assumes the script is run as root.
     """
-    is_root = os.geteuid() == 0
 
-    def __init__(self, target_user: str = None, sudo_loop: bool = True):
+    def __init__(self, target_uname: str = None):
         """
-        :param target_user: The user to run commands under.
-                            Only allowed if the script is root.
-        :param sudo_loop:   Whether script should refresh sudo calls to avoid sudo timeout.
+        :param target_uname: None if the script does not have a target user; applies when running
+                            directly as root user.
+                            Expects a valid dictionary containing:
+                                uname: str, the target user name
+                                gid: int, the target user group id
+                                uid: int, the target user id
         """
-        log('Initializing runner object.', logging.INFO)
-        if target_user and target_user != getpass.getuser():
-            assert self.is_root, 'Setting target users is only allowed if the script is run as root.'
+        log('Initializing runner.', logging.INFO)
+        if target_uname:
             try:
-                info = pwd.getpwnam(target_user)
+                info = pwd.getpwnam(target_uname)
             except KeyError:
-                print('User %s does not exist.' % target_user)
+                log(f'User {target_uname} does not exist.', logging.ERROR)
                 raise
             self.target_user = dict(
-                uname=target_user,
+                uname=target_uname,
                 gid=info.pw_gid,
-                uid=info.pw_uid,
-                env=os.environ.copy()
-            )
-            self.target_user['env'].update({'HOME': info.pw_dir, 'LOGNAME': target_user, 'USER': target_user})
+                uid=info.pw_uid)
         else:
             self.target_user = None
-        self.has_target_user = self.target_user is not None
-        if sudo_loop:
-            self.sudo_loop()
         log(f'Runner object was created with the following settings:\n{str(self)}', logging.DEBUG)
 
-    @staticmethod
-    def sudo_loop():
-        """
-        Once called, loop sudo validate every 5 seconds until parent process dies.
-        Sudo may prompt for a password at call start.
+    # @staticmethod
+    # def sudo_loop():
+    #     """
+    #     Once called, loop sudo validate every 5 seconds until parent process dies.
+    #     Sudo may prompt for a password at call start.
+    #
+    #     Raises PermissionError if the first sudo call is not successful.
+    #     """
+    #     log('Starting sudo loop.', logging.INFO)
+    #     # FIX SUDO COMMENTS AND PLACEHOLDER ECHOS WHEN NOT A NUISANCE
+    #
+    #     # p = Popen(['sudo', '-v'])
+    #     # p.communicate()
+    #     # if p.returncode != 0:
+    #     #   raise PermissionError('sudo could not validate.')
+    #
+    #     def sudo_loop_thread():
+    #         i = 0.0
+    #         while True:
+    #             if not threading.main_thread().is_alive():
+    #                 log('Exiting sudo_loop_thread.', logging.DEBUG)
+    #                 break
+    #             if i >= 5.0:
+    #                 log('Revalidating sudo.', logging.DEBUG)
+    #                 # Popen(['sudo', '-v']).wait()
+    #                 i = 0.0
+    #             else:
+    #                 time.sleep(0.5)
+    #                 i += 0.5
+    #     threading.Thread(target=sudo_loop_thread).start()
+    #     log('Started sudo_loop_thread.', logging.DEBUG)
 
-        Raises PermissionError if the first sudo call is not successful.
-        """
-        log('Starting sudo loop.', logging.INFO)
-        # TODO: FIX SUDO COMMENTS AND PLACEHOLDER ECHOS WHEN NOT A NUISANCE
-
-        # p = Popen(['sudo', '-v'])
-        # p.communicate()
-        # if p.returncode != 0:
-        #   raise PermissionError('sudo could not validate.')
-
-        def sudo_loop_thread():
-            i = 0.0
-            while True:
-                if not threading.main_thread().is_alive():
-                    log('Exiting sudo_loop_thread.', logging.DEBUG)
-                    break
-                if i >= 5.0:
-                    log('Revalidating sudo.', logging.DEBUG)
-                    # Popen(['sudo', '-v']).wait()
-                    i = 0.0
-                else:
-                    time.sleep(0.5)
-                    i += 0.5
-        threading.Thread(target=sudo_loop_thread).start()
-        log('Started sudo_loop_thread.', logging.DEBUG)
-
-    def run(self, cmd: str, args: list[str] = None) -> (int, str):
+    def run(self, cmd: str, args: list[str] = None) -> int:
         """
         Run the given command(s) through the shell along with any arguments.
 
@@ -90,35 +79,22 @@ class Runner:
         """
         log(f'Running cmd:\n{cmd}', logging.DEBUG)
         args = args or []
-        p = None
         try:
-            if self.target_user:
-                def set_ids():
+            def set_ids():
+                if self.target_user:
                     os.initgroups(self.target_user['uname'], self.target_user['gid'])
                     os.setuid(self.target_user['uid'])
-
-                p = Popen([cmd, ''] + args,
-                          preexec_fn=set_ids, stderr=PIPE, universal_newlines=True, shell=True, env=self.target_user['env'])
-            else:
-                p = Popen([cmd, ''] + args, stderr=PIPE, universal_newlines=True, shell=True)
-            _, error = p.communicate()
-            if p.returncode != 0:
-                log(f'The following command exited with a non-zero returncode:\n'
-                    f'{cmd}\n'
-                    f'exit code {p.returncode}\n'
-                    f'stderr {error}', logging.WARNING)
+            try:
+                subprocess.run([cmd, ''] + args, preexec_fn=set_ids, check=True, text=True, shell=True)
+            except subprocess.CalledProcessError as error:
+                log(f'Encountered a CalledProcessError exception:\n'
+                    f'{error}\n', logging.ERROR)
+                return error.returncode
+            return 0
         except KeyboardInterrupt:
-            if p:
-                log('Terminating run process.', logging.DEBUG)
-                p.terminate()
             log('Exiting due to KeyboardInterrupt', logging.DEBUG)
             return exit('\nAborting.')
-        return p.returncode
 
     def __str__(self):
-        rtn = '\n'.join([
-            f'is_root: {self.is_root}',
-            f'target_user: {self.target_user}',
-            f'has_target_user: {self.has_target_user}'
-        ])
+        rtn = f'target_user: {self.target_user}'
         return rtn
