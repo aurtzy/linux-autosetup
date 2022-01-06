4f0affc9d4781514d8e0d079d382a706f98d0325
aurtzy <65694715+aurtzy@users.noreply.github.com>
2022-01-05 23:16:43 -0500

    create prompter module for more universally formatted prompting

---
+++ linux-autosetup/lib/prompter.py
@@ -0,0 +1,37 @@
+import logging
+
+from lib.logger import log
+
+
+def get_input(options: list[list[str]], pre_prompt: str = ''):
+    """
+    Provides a universal method of getting decently formatted prompts from the user.
+
+    Expects a list of tuples, in which each tuple contains an option.
+
+    The first element of the tuple will be considered the message to be attached, while
+    any elements after that are assumed as alternative accepted inputs that are associated with
+    that option. Every option in the list is automatically tagged with their respective natural number in the list.
+
+    :returns: The natural number position of the first found option which matches user input.
+    """
+    if not options:
+        log('get_input only expects a non-empty list of options... Exiting.', logging.ERROR)
+        exit(1)
+    log(f'Attempting to get input from the user with the following:\n'
+        f'pre_prompt: {pre_prompt}\n'
+        f'options: {options}', logging.DEBUG)
+
+    formatted_prompt = '\n'.join(f'{i} [{"/".join(option[1:])}] {option[0]}' for i, option in enumerate(options))
+
+    while True:
+        log('Prompting user.', logging.DEBUG)
+        if pre_prompt:
+            print(pre_prompt)
+        print(formatted_prompt)
+        user_in = input('Please choose one of the options above: ')
+        for i, option in enumerate(options):
+            if user_in in option[1:] or user_in == str(i):
+                log(f'Accepting input {user_in} and returning {i}.', logging.DEBUG)
+                return i
+        log(f'Could not find any option matching input. Try again.', logging.INFO)
