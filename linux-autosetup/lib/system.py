668e319edc7e977717d4172c74f073bf8964c90f
aurtzy <65694715+aurtzy@users.noreply.github.com>
2022-01-07 18:13:15 -0500

    rename runner module to system module to more accurately reflect contents; add Path class

---
+++ linux-autosetup/lib/system.py
@@ -0,0 +1,129 @@
+import logging
+import os
+import subprocess
+import threading
+import time
+
+from lib.logger import log
+
+
+def sudo_loop():
+    """
+    Mitigates potential issues when running prolonged commands that may cause the sudo
+    timeout to expire, resulting in sudo prompts during the script.
+
+    Should only be called at most once within script.
+
+    Once called, loop sudo -v every 5 seconds until parent process (the caller) ends.
+    Sudo may prompt for a password at call start.
+
+    Raises PermissionError if the first sudo call is not successful.
+    """
+    log('Starting sudo loop.', logging.INFO)
+    # TODO: FIX SUDO COMMENTS WHEN NOT A NUISANCE
+    # p = Popen(['sudo', '-v'])
+    # p.communicate()
+    # if p.returncode != 0:
+    #   raise PermissionError('sudo could not validate.')
+
+    def sudo_loop_thread():
+        i = 0.0
+        while True:
+            if not threading.main_thread().is_alive():
+                log('Exiting sudo_loop_thread.', logging.DEBUG)
+                break
+            if i >= 5.0:
+                log('Revalidating sudo.', logging.DEBUG)
+                # Popen(['sudo', '-v']).communicate()
+                i = 0.0
+            else:
+                time.sleep(0.5)
+                i += 0.5
+    threading.Thread(target=sudo_loop_thread).start()
+    log('Started sudo_loop_thread.', logging.DEBUG)
+
+
+def run(cmd: str, args: list[str] = None) -> int:
+    """
+    Run the given command(s) through the shell along with any arguments.
+
+    :param cmd:     String of command(s) to run.
+    :param args:    Optional arguments to supply to the shell.
+    :return:        Return-code of running command(s).
+    """
+    log(f'Running command(s):\n{cmd}', logging.DEBUG)
+    args = args or []
+    try:
+        subprocess.run([cmd, ''] + args, check=True, text=True, shell=True)
+    except subprocess.CalledProcessError as error:
+        log(f'Encountered an error running shell commands:\n'
+            f'{error}\n', logging.ERROR)
+        return error.returncode
+    return 0
+
+
+class Path(os.PathLike):
+    """
+    Implements os.PathLike for use with packs.
+
+    Provides methods to interact with files on the system.
+
+    su_cmd: str
+        Superuser command, used to elevate commands to make sure they have necessary permissions to function.
+        Uses 'sudo' by default.
+
+    The following variables are configured for use with GNU/Linux systems, but may be changed to work for
+    other systems instead:
+
+    cp_cmd: str
+        Copy command, used for copying files to locations.
+    mv_cmd: str
+        Move command, used for moving files to locations.
+    mkdir_cmd: str
+        Make directory command, used for creating directories.
+    """
+
+    su_cmd: str = 'sudo'
+
+    cp_cmd: str = 'cp -at "$1" "${@:2}"'
+    mv_cmd: str = 'mv -t "$1" "${@:2}'
+    mkdir_cmd: str = 'mkdir -p $1'
+
+    def __init__(self, path):
+        self.path = os.path.expandvars(str(path))
+
+    @staticmethod
+    def copy(dest: os.PathLike, *args: os.PathLike) -> bool:
+        """
+        Copies files from the given path(s) args to dest.
+
+        :return: True if the copy was successful; False otherwise.
+        """
+        print(f'TEMP: copy {", ".join(path.__fspath__() for path in args)} to {dest.__fspath__()}')
+        return True
+
+    @staticmethod
+    def move(dest: os.PathLike, *args: os.PathLike) -> bool:
+        """
+        Moves files from the given path(s) args to dest.
+
+        :return: True if the move was successful; False otherwise.
+        """
+        print(f'TEMP: move {", ".join(path.__fspath__() for path in args)} to {dest.__fspath__()}')
+        return True
+
+    @staticmethod
+    def mkdir(path: os.PathLike) -> bool:
+        """
+        Creates directory if it doesn't exist at the specified path.
+
+        :return: True if the directory creation was successful or if it already exists; False otherwise.
+        """
+        print(f'TEMP: Create directory to path {path.__fspath__()} if it doesn\'t exist.')
+        return True
+
+    def __fspath__(self):
+        return self.path
+
+    def __str__(self):
+        return self.path
