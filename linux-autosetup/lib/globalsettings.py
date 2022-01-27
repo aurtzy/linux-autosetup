40520c7e8c4ef6daa6a154f5d3af17a233b5941c
aurtzy <65694715+aurtzy@users.noreply.github.com>
2022-01-26 23:49:17 -0500

    reimplement and build foundation for globalsettings; implement update method

--- linux-autosetup/lib/globalsettings.py
+++ linux-autosetup/lib/globalsettings.py
@@ -1,125 +1,161 @@
 import logging
-from collections.abc import Mapping
-from typing import TypedDict
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, is_dataclass, field
 
 from lib.logger import log
 
 
-class AppsModuleSettings(TypedDict):
-    install_cmd_types: dict[str, str]
+@dataclass
+class Settings:
 
+    class Nested(ABC):
+        @staticmethod
+        @abstractmethod
+        def from_dict(d: dict):
+            pass
 
-class FilesModuleSettings(TypedDict):
-    install_cmd_types: dict[str, str]
-    backup_cmd_types: dict[str, str]
+    @dataclass
+    class CmdPreset(Nested):
+        install_cmd: str = ''
+        backup_cmd: str = ''
 
+        @staticmethod
+        def from_dict(d: dict) -> 'Settings.CmdPreset':
+            return Settings.CmdPreset(
+                install_cmd=d.get('install_cmd', ''),
+                backup_cmd=d.get('backup_cmd', '')
+            )
 
-class SystemCmdsSettings(TypedDict):
-    superuser: str
-    cp: str
-    mv: str
-    mkdir: str
-    validate_path: str
-    validate_dir: str
+    def update(self, new: dict):
+        log(f'Updating {self.__class__.__name__} with:\n'
+            f'{new}', logging.DEBUG)
 
+        def log_update_error(s, ignored):
+            log(f'Encountered an unexpected value for {s}:\n'
+                f'Ignoring setting {ignored}', logging.ERROR)
+        for setting, tp in self.__annotations__.items():
+            if new.get(setting) is None:
+                continue
 
-class GlobalSettings(TypedDict):
-    noconfirm: bool
-    apps_module: AppsModuleSettings
-    files_module: FilesModuleSettings
-    system_cmds: SystemCmdsSettings
-
-
-global_settings: GlobalSettings = {
-    'noconfirm': False,
-    'apps_module': {
-        'install_cmd_types': {
-            'flatpak': 'flatpak install -y --noninteractive $@',
-            'apt': 'sudo apt --assume-yes install $@',
-            'pacman': 'sudo pacman -S --noconfirm --needed $@',
-            'yay': 'yay -S --noconfirm --needed $@'
-        }
-    },
-    'files_module': {
-        'install_cmd_types': {
-            'copy': '',
-            'hardlink': '',
-            'tar_copy': 'tar -xPf "$1.tar"',
-            'compress': 'tar -xPf "$1.tar.xz"',
-            'encrypt': 'openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -salt -in "$1.tar.xz.enc" | tar -xPf -'
-        },
-        'backup_cmd_types': {
-            'copy': '',
-            'hardlink': '',
-            'tar_copy': 'tar -cPf "$1.tar" "${@:2}"',
-            'compress': 'tar -cJPf "$1.tar.xz" "${@:2}"',
-            'encrypt': 'tar -cJPf - "${@:2}" | '
-                       'openssl enc -e -aes-256-cbc -md sha512 -pbkdf2 -salt -out "$1.tar.xz.enc"'
-        }
-    },
-    'system_cmds': {
-        'superuser': 'sudo',
-        'cp': 'cp -at "$1" "${@:2}"',
-        'mv': 'mv -t "$1" "${@:2}"',
-        'mkdir': 'mkdir -p "$1"',
-        'validate_path': '[ -e "$1" ]',
-        'validate_dir': '[ -d "$1" ]'
-    }
-}
-
-
-def update_global_settings(new: dict):
-    """
-    Preferred over using something like dict's update() which may undesirably remove predefined settings as
-    a result of its shallow updating.
-
-    Manually updates each setting in global_settings individually in order to accurately filter
-    user-typed configurations and make sure no significant errors are present before
-    the script uses them.
-    """
-
-    def update(*args, t):
-        """
-        Updates the given setting. Each arg represents one level into the settings.
-
-        If the type is dict,
-        """
-        setting = args[-1]
-        log(f'Attempting to update {setting}.', logging.DEBUG)
-
-        g_s = global_settings
-        for arg in args[:-1]:
-            g_s = g_s[arg]
-        n = new
-        for arg in args[:-1]:
-            if not isinstance(n.get(arg), Mapping):
-                log(f'Could not find path to {setting}. Nothing to change here.', logging.INFO)
-                return
-            n = n[arg]
-
-        if n.get(setting) is None:
-            log(f'{setting} is not present. Nothing to change here.', logging.INFO)
-        else:
-            if isinstance(n[setting], t):
-                if t is dict:
-                    g_s[setting].update(n[setting])
+            if tp.__subclasscheck__(dict):
+                try:
+                    dict_args: tuple = tp.__args__
+                except AttributeError:
+                    dict_args: tuple = ()
+                log(f'dict_args: {dict_args}', logging.DEBUG)
+                if isinstance(new.get(setting), dict):
+                    log(f'Updating {setting}: {new.get(setting)}',logging.INFO)
+                    for k, v in new.get(setting).items():
+                        if len(dict_args) > 0:
+                            k = dict_args[0](k)
+                            if len(dict_args) > 1:
+                                if is_dataclass(dict_args[1]):
+                                    if isinstance(v, dict):
+                                        v = dict_args[1].from_dict(v)
+                                    else:
+                                        log_update_error(setting, v)
+                                else:
+                                    v = dict_args[1](v)
+                        self.__dict__[setting][k] = v
+            elif is_dataclass(tp):
+                if isinstance(new.get(setting), dict):
+                    log(f'Updating {setting}...', logging.INFO)
+                    self.__dict__[setting].update(new.get(setting))
                 else:
-                    g_s[setting] = n.get(setting)
-                log(f'Updating {setting} to be {g_s[setting]}.', logging.INFO)
-
+                    log_update_error(setting, new.get(setting))
             else:
-                log(f'Could not match {n[setting]} with {setting}\'s type. This setting will be ignored.',
-                    logging.WARNING)
+                if isinstance(new.get(setting), tp):
+                    log(f'Updating {setting}: {new.get(setting)}', logging.INFO)
+                    self.__dict__[setting] = new.get(setting)
+                else:
+                    log_update_error(setting, new.get(setting))
 
-    # noconfirm
-    update('noconfirm', t=bool)
 
-    # apps_module
-    update('apps_module', 'install_cmd_types', t=dict)
+@dataclass
+class GlobalSettings(Settings):
 
-    # files_module
-    update('files_module', 'install_cmd_types', t=dict)
-    update('files_module', 'backup_cmd_types', t=dict)
+    @dataclass
+    class CustomModule(Settings):
+        cmd_presets: dict[str, Settings.CmdPreset] = field(default_factory=dict)
 
-    # path_ops
-    update('path_ops', t=dict)
+    @dataclass
+    class AppsModule(CustomModule):
+        pass
+
+    @dataclass
+    class FilesModule(CustomModule):
+        pass
+
+    @dataclass
+    class SystemCmds(Settings):
+        """
+        Preconfigured to use POSIX-compatible GNU/Linux shell commands.
+        """
+        superuser: str
+        cp: str
+        mv: str
+        mkdir: str
+        validate_path: str
+        validate_dir: str
+
+    noconfirm: bool
+    system_cmds: SystemCmds
+    custom_module: CustomModule
+    apps_module: AppsModule
+    files_module: FilesModule
+
+    def __str__(self):
+        return str(self.__dict__)
+
+
+global_settings = GlobalSettings(
+    noconfirm=False,
+    custom_module=GlobalSettings.CustomModule(cmd_presets={}),
+    apps_module=GlobalSettings.AppsModule(
+        cmd_presets={
+            'flatpak': Settings.CmdPreset(
+                install_cmd='flatpak install -y --noninteractive $@'
+            ),
+            'apt': Settings.CmdPreset(
+                install_cmd='sudo apt --assume-yes install $@'
+            ),
+            'pacman': Settings.CmdPreset(
+                install_cmd='sudo pacman -S --noconfirm --needed $@'
+            ),
+            'yay': Settings.CmdPreset(
+                install_cmd='yay -S --noconfirm --needed $@'
+            )
+        }
+    ),
+    files_module=GlobalSettings.FilesModule(
+        cmd_presets={
+            'copy': Settings.CmdPreset(
+
+            ),
+            'hardlink': Settings.CmdPreset(
+
+            ),
+            'tar_copy': Settings.CmdPreset(
+                install_cmd='tar -xPf "$1.tar"',
+                backup_cmd='tar -cPf "$1.tar" "${@:2}"'
+            ),
+            'compress': Settings.CmdPreset(
+                install_cmd='tar -xPf "$1.tar.xz"',
+                backup_cmd='tar -cJPf "$1.tar.xz" "${@:2}"'
+            ),
+            'encrypt': Settings.CmdPreset(
+                install_cmd='openssl enc -d -aes-256-cbc -md sha512 -pbkdf2 -salt -in "$1.tar.xz.enc" | tar -xPf -',
+                backup_cmd='tar -cJPf - "${@:2}" | '
+                            'openssl enc -e -aes-256-cbc -md sha512 -pbkdf2 -salt -out "$1.tar.xz.enc"'
+            )
+        }
+    ),
+    system_cmds=GlobalSettings.SystemCmds(
+        superuser='sudo',
+        cp='cp -at "$1" "${@:2}"',
+        mv='mv -t "$1" "${@:2}"',
+        mkdir='mkdir -p "$1"',
+        validate_path='[ -e "$1" ]',
+        validate_dir='[ -d "$1" ]'
+    )
+)
