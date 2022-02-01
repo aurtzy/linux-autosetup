b543810f84c340dffbcffcc2e05e8b0ca69b10c8
aurtzy <65694715+aurtzy@users.noreply.github.com>
2022-01-31 23:07:42 -0500

    use run method instead of Popen

--- linux-autosetup/lib/system.py
+++ linux-autosetup/lib/system.py
@@ -10,44 +10,6 @@ from lib.logger import log
 from lib.prompter import get_input
 
 
-def sudo_loop():
-    """
-    Mitigates potential issues when running prolonged commands that may cause the sudo
-    timeout to expire, resulting in sudo prompts during the script.
-
-    Should only be called at most once within script.
-
-    Once called, loop sudo -v every 5 seconds until parent process (the caller) ends.
-    Sudo may prompt for a password at call start.
-
-    Raises PermissionError if the first sudo call is not successful.
-    """
-    log('Starting sudo loop.', logging.INFO)
-
-    # TODO: FIX SUDO COMMENTS WHEN NOT A NUISANCE
-    # p = Popen(['sudo', '-v'])
-    # p.communicate()
-    # if p.returncode != 0:
-    #   raise PermissionError('sudo could not validate.')
-
-    def sudo_loop_thread():
-        i = 0.0
-        while True:
-            if not threading.main_thread().is_alive():
-                log('Exiting sudo_loop_thread.', logging.DEBUG)
-                break
-            if i >= 5.0:
-                log('Revalidating sudo.', logging.DEBUG)
-                # Popen(['sudo', '-v']).communicate()
-                i = 0.0
-            else:
-                time.sleep(0.5)
-                i += 0.5
-
-    threading.Thread(target=sudo_loop_thread).start()
-    log('Started sudo_loop_thread.', logging.DEBUG)
-
-
 def run(cmd: str, args: list[str] = None) -> int:
     """
     Run the given command(s) through the shell along with any arguments.
@@ -68,6 +30,42 @@ def run(cmd: str, args: list[str] = None) -> int:
             f'{error}\n', logging.ERROR)
         return error.returncode
     return 0
+
+
+def sudo_loop():
+    """
+    Mitigates potential issues when running prolonged commands that may cause the sudo
+    timeout to expire, resulting in sudo prompts during the script.
+
+    Should only be called at most once within script.
+
+    Once called, loop sudo -v every 5 seconds until parent process (the caller) ends.
+    Sudo may prompt for a password at call start.
+
+    Raises PermissionError if the first sudo call is not successful.
+    """
+    log('Starting sudo loop.', logging.INFO)
+
+    # TODO: FIX SUDO COMMENTS WHEN NOT A NUISANCE
+    # if run('sudo -v') != 0:
+    #     raise PermissionError('sudo could not validate.')
+
+    def sudo_loop_thread():
+        i = 0.0
+        while True:
+            if not threading.main_thread().is_alive():
+                log('Exiting sudo_loop_thread.', logging.DEBUG)
+                break
+            if i >= 5.0:
+                log('Revalidating sudo.', logging.DEBUG)
+                # run('sudo -v')
+                i = 0.0
+            else:
+                time.sleep(0.5)
+                i += 0.5
+
+    threading.Thread(target=sudo_loop_thread).start()
+    log('Started sudo_loop_thread.', logging.DEBUG)
 
 
 class Path(PathLike):
