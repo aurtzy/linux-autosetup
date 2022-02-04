f6ffe53cd558eeea954bfba26202506ea69f9fc6
aurtzy <65694715+aurtzy@users.noreply.github.com>
2022-02-04 02:34:00 -0500

    implement overriding global options; progress on config parser

--- linux-autosetup/lib/configparser.py
+++ linux-autosetup/lib/configparser.py
@@ -1,8 +1,16 @@
+import logging
+from os import PathLike
+
+from ruamel.yaml import YAML, YAMLError
+
+from lib.logger import log
+from lib.settings import global_settings
+
 
 class ConfigParser:
 
-    def __init__(self, path: str):
-        self.path = path
+    def __init__(self, config_path: PathLike):
+        self.config_path = config_path
 
     def start(self, option_overrides: dict = None):
         """
@@ -12,11 +20,45 @@ class ConfigParser:
                                  global_settings['options'] and override the original value from the config file.
                                  This lets the script prefer script arguments over config settings.
         """
-        # TODO: parse config here!!
-        pass
+        if option_overrides is None:
+            option_overrides = {}
 
-    def parse_global_settings(self, g_s: dict):
-        """Specifically parses for and updates global_settings from the given dict g_s."""
+        yaml = YAML(typ='safe')
+        log('Reading config file...', logging.DEBUG)
+        try:
+            with open(self.config_path, 'r') as file:
+                config: dict = yaml.load(file)
+                file.close()
+        except YAMLError as error:
+            log('Encountered an error reading config.', logging.CRITICAL)
+            log(str(error), logging.CRITICAL)
+            exit(1)
+        log(str(config), logging.DEBUG)
+
+        try:
+            assert isinstance(config, dict)
+        except AssertionError:
+            if config is None:
+                log(f'Could not find anything in {self.config_path}. Exiting...',
+                    logging.ERROR)
+            else:
+                log(f'Encountered an unexpected error trying to parse {self.config_path}. Exiting...',
+                    logging.ERROR)
+            exit(1)
+
+        log('Parsing global settings...', logging.DEBUG)
+        self.parse_settings(config.get('global_settings'))
+
+        log('Checking for any global options to override...', logging.DEBUG)
+        global_settings.options.__dict__.update(option_overrides)
+        for option, val in option_overrides.items():
+            log(f'Overriding {option}: {val}', logging.INFO)
+
+        log('Parsing packs...', logging.DEBUG)
+        self.parse_packs(config.get('packs'))
+
+    def parse_settings(self, s: dict):
+        """Specifically parses for and updates global_settings from the given dict s."""
         # TODO: parse given dictionary of global settings
         pass
 
--- linux-autosetup/testing.py
+++ linux-autosetup/testing.py
@@ -1,12 +1,15 @@
-import yaml
+import logging
+
+from ruamel.yaml import YAML, YAMLError
+
+from lib.configparser import ConfigParser
+from lib.logger import log
+from lib.system import Path
 
 if __name__ == '__main__':
 
-    # can be set to be more verbose if a --debug type option is set
-    # logging.basicConfig(format='%(message)s', level=logging.INFO)
-    try:
-        with open('sample-configs/config.yaml', 'r') as file:
-            config = yaml.safe_load(file)
-        file.close()
-    except yaml.YAMLError as error:
-        raise yaml.YAMLError('There was an error reading config.') from error
+    config_path = Path('sample-configs/config.yaml')
+
+    config_parser = ConfigParser(config_path)
+
+    config_parser.start()
